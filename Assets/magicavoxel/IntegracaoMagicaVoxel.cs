8eda2bf6d47b0cea192d003eebef788d69c5c393
Nicolas <nicolas.andalaft@hotmail.com>
2020-03-23 22:18:23 -0300

    Adicionado namespace nos scripts

--- Assets/magicavoxel/IntegracaoMagicaVoxel.cs
+++ Assets/magicavoxel/IntegracaoMagicaVoxel.cs
@@ -5,92 +5,95 @@ using UnityEditor;
 using System.IO;
 using System;
 
-public class IntegracaoMagicaVoxel : MonoBehaviour
+namespace IntegracaoMagicaVoxel
 {
-    /// <summary>
-    /// Lê um arquivo .ply exportado no modo "CloudPoint" pelo MagicaVoxel (não testado em
-    /// outros programas) e gera o cenário na Unity, usando as cores dos blocos como códigos
-    /// para a substituição pelos modelos referentes
-    /// </summary>
-
-    public static void PopularDicionario(ref Dictionary<string, string> dicionario, string path)
+    public class IntegracaoMagicaVoxel : MonoBehaviour
     {
-        var arqRef = new StreamReader(path);
+        /// <summary>
+        /// Lê um arquivo .ply exportado no modo "CloudPoint" pelo MagicaVoxel (não testado em
+        /// outros programas) e gera o cenário na Unity, usando as cores dos blocos como códigos
+        /// para a substituição pelos modelos referentes
+        /// </summary>
 
-        try
+        public static void PopularDicionario(ref Dictionary<string, string> dicionario, string path)
         {
+            var arqRef = new StreamReader(path);
+
+            try
+            {
+                string[] linha;
+                do
+                {
+                    linha = arqRef.ReadLine().Split(':');
+                    dicionario.Add(linha[0], linha[1]);
+
+                } while (!arqRef.EndOfStream);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError("Erro ao analizar o arquivo de dicionário para o MagicaVoxel\n" + e);
+            }
+        }
+
+        public static void GerarCena(ref Dictionary<string, string> dicionario, string path)
+        {
+            Debug.Log("Gerando a Cena...");
+            // ----------------------------------
+            //
+
+            var arqPly = new StreamReader(path);
+
+            Transform paiCenario = new GameObject("Cenario Importado").transform;
+
+            // Pula o cabeçalho do arquivo
+            while (arqPly.ReadLine() != "end_header") ;
+
+            // Lê a disposição dos cubos
             string[] linha;
             do
             {
-                linha = arqRef.ReadLine().Split(':');
-                dicionario.Add(linha[0], linha[1]);
+                linha = arqPly.ReadLine().Split(' ');
 
-            } while (!arqRef.EndOfStream);
-        }
-        catch (System.Exception e)
-        {
-            Debug.LogError("Erro ao analizar o arquivo de dicionário para o MagicaVoxel\n" + e);
-        }
-    }
+                // Popular posição
+                Vector3 posi = new Vector3();
+                posi.x = int.Parse(linha[0]);
+                posi.z = int.Parse(linha[1]);
+                posi.y = int.Parse(linha[2]);
 
-    public static void GerarCena(ref Dictionary<string, string> dicionario, string path)
-    {
-        Debug.Log("Gerando a Cena...");
-        // ----------------------------------
-        //
-            
-        var arqPly = new StreamReader(path);
+                // Popular chave
+                var chave = String.Format("{0} {1} {2}", linha[3], linha[4], linha[5]);
 
-        Transform paiCenario = new GameObject("Cenario Importado").transform;
-
-        // Pula o cabeçalho do arquivo
-        while (arqPly.ReadLine() != "end_header") ;
-
-        // Lê a disposição dos cubos
-        string[] linha;
-        do
-        {
-            linha = arqPly.ReadLine().Split(' ');
-
-            // Popular posição
-            Vector3 posi = new Vector3();
-            posi.x = int.Parse(linha[0]);
-            posi.z = int.Parse(linha[1]);
-            posi.y = int.Parse(linha[2]);
-
-            // Popular chave
-            var chave = String.Format("{0} {1} {2}", linha[3], linha[4], linha[5]);
-
-            // Verifica se a chave está presente no dicionário
-            string nomePrefab;
-            if (dicionario.ContainsKey(chave))
-            {
-                nomePrefab = dicionario[chave];
-
-                if (nomePrefab != "vazio")
+                // Verifica se a chave está presente no dicionário
+                string nomePrefab;
+                if (dicionario.ContainsKey(chave))
                 {
-                    try
+                    nomePrefab = dicionario[chave];
+
+                    if (nomePrefab != "vazio")
                     {
-                        // Instancia Prefab na posição correta
-                        GameObject obj = Instantiate(
-                            Resources.Load(@"magicavoxel/prefabs/" + nomePrefab)) as GameObject;
-                        obj.transform.position = posi;
-                        obj.transform.parent = paiCenario;
+                        try
+                        {
+                            // Instancia Prefab na posição correta
+                            GameObject obj = Instantiate(
+                                Resources.Load(@"magicavoxel/prefabs/" + nomePrefab)) as GameObject;
+                            obj.transform.position = posi;
+                            obj.transform.parent = paiCenario;
+                        }
+
+                        // Mensagem de erro
+                        catch { Debug.LogError("Prefab não encontrado: " + nomePrefab); }
                     }
-
-                    // Mensagem de erro
-                    catch { Debug.LogError("Prefab não encontrado: " + nomePrefab); }
                 }
-            }
 
-            // Mensagem de erro
-            else
-                Debug.LogError("Chave não encontrada no dicionário: " + chave);
+                // Mensagem de erro
+                else
+                    Debug.LogError("Chave não encontrada no dicionário: " + chave);
 
-        } while (!arqPly.EndOfStream);
+            } while (!arqPly.EndOfStream);
 
-        //
-        // ----------------------------------
-        Debug.Log("Cena gerada!");
+            //
+            // ----------------------------------
+            Debug.Log("Cena gerada!");
+        }
     }
 }
--- Assets/magicavoxel/MenuMagicaVoxel.cs
+++ Assets/magicavoxel/MenuMagicaVoxel.cs
@@ -2,34 +2,37 @@
 using UnityEditor;
 using System.Collections.Generic;
 
-public class MenuMagicaVoxel : EditorWindow
+namespace IntegracaoMagicaVoxel
 {
-    static Dictionary<string, string> dicionario = new Dictionary<string, string>();
-    static Object pathPly, pathDicio;
-
-    [MenuItem("MagicaVoxel/Gerar Cena")]
-    public static void GerarCena()
+    public class MenuMagicaVoxel : EditorWindow
     {
-        string auxPath, fullPath;
-        auxPath = Application.dataPath;
+        static Dictionary<string, string> dicionario = new Dictionary<string, string>();
+        static Object pathPly, pathDicio;
 
-        fullPath = auxPath + AssetDatabase.GetAssetPath(pathDicio).Remove(0, 6);
-        IntegracaoMagicaVoxel.PopularDicionario(ref dicionario, fullPath);
+        [MenuItem("MagicaVoxel/Gerar Cena")]
+        public static void GerarCena()
+        {
+            string auxPath, fullPath;
+            auxPath = Application.dataPath;
 
-        fullPath = auxPath + AssetDatabase.GetAssetPath(pathPly).Remove(0, 6);
-        IntegracaoMagicaVoxel.GerarCena(ref dicionario, fullPath);
-    }
+            fullPath = auxPath + AssetDatabase.GetAssetPath(pathDicio).Remove(0, 6);
+            IntegracaoMagicaVoxel.PopularDicionario(ref dicionario, fullPath);
 
-    [MenuItem("MagicaVoxel/Configurações")]
-    public static void mostrarJanela()
-    {
-        GetWindow<MenuMagicaVoxel>("Configurações Integração MagicaVoxel");
-    }
+            fullPath = auxPath + AssetDatabase.GetAssetPath(pathPly).Remove(0, 6);
+            IntegracaoMagicaVoxel.GerarCena(ref dicionario, fullPath);
+        }
 
-    private void OnGUI()
-    {
-        pathPly = EditorGUILayout.ObjectField(pathPly, typeof(Object), false);
-        pathDicio = EditorGUILayout.ObjectField(pathDicio, typeof(Object), false);
-        GUILayout.Label(Application.dataPath + AssetDatabase.GetAssetPath(pathDicio));
+        [MenuItem("MagicaVoxel/Configurações")]
+        public static void mostrarJanela()
+        {
+            GetWindow<MenuMagicaVoxel>("Configurações Integração MagicaVoxel");
+        }
+
+        private void OnGUI()
+        {
+            pathPly = EditorGUILayout.ObjectField(pathPly, typeof(Object), false);
+            pathDicio = EditorGUILayout.ObjectField(pathDicio, typeof(Object), false);
+            GUILayout.Label(Application.dataPath + AssetDatabase.GetAssetPath(pathDicio));
+        }
     }
 }
