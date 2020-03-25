ab4af9ef19b28bdf1f7ee2b1ed6553546fc19ce4
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-03-25 00:04:05 -0300

    estados de cliente, cada um  no final remove a si mesmo e adiciona o próximo
    o único parcialmente implementado é o AndaNaFila, preciso terminar isso
    amanhã.

---
+++ Assets/codigos/componentes/estados cliente/AndaNaFila.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndaNaFila : MonoBehaviour
+{
+    Fila fila;
+    static int ultimoIDFila = 1;
+
+    int id, espaco_i;
+
+    Transform pontoFila;
+
+    ControladorVaiAtePonto ctrlVaiPonto;
+
+    void ObtemEspaco() {
+        espaco_i = fila.RegistraEspaco(id);
+        if (espaco_i >= 0) {
+            pontoFila = fila.fila_trs[espaco_i];
+            ctrlVaiPonto.trAlvo = pontoFila;
+        }
+    }
+
+    void Awake() {
+        fila = FindObjectOfType<Fila>();
+        ctrlVaiPonto = GetComponent<ControladorVaiAtePonto>();
+        id = ++ultimoIDFila;
+    }
+
+    void Start() {
+        ObtemEspaco();
+    }
+
+    void Update() {
+        espaco_i = fila.ObtemIndicePorID(id, espaco_i);
+
+        if (espaco_i >= 0) {
+            if (pontoFila != fila.fila_trs[espaco_i])
+                ctrlVaiPonto.trAlvo = pontoFila;
+        } else
+            ObtemEspaco();
+    }
+}
