f2f3c06a071d0ce8b088848938722df49a9a154c
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-03-25 00:02:42 -0300

    o estado do jogo estará dividido entre vários componentes
    começando com Fila e Cadeiras, Fila guarda o estado da fila, Cadeiras
    guaradá o estado das cadeiras ocupadas ou não.

---
+++ Assets/codigos/componentes/estado de jogo/Fila.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fila : MonoBehaviour
+{
+    public Transform[] fila_trs;
+    int[] espacosOcupados;
+
+    void Awake() {
+        espacosOcupados = new int[fila_trs.Length];
+    }
+
+    public int RegistraEspaco(int id) {
+        for (int i = 0; i < espacosOcupados.Length; i++) {
+            if (espacosOcupados[i] == 0) {
+                espacosOcupados[i] = id;
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public int ObtemIndicePorID(int id, int i) {
+        if (espacosOcupados[i] == id)
+            return i;
+        for (int x = 0; x < espacosOcupados.Length; x++) {
+            if (espacosOcupados[x] == id)
+                return i;
+        }
+        return -1;
+    }
+}
