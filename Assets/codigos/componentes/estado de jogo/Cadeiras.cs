da199c60913fcc9078964f603cd626fa26090fdf
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-03-30 11:18:03 -0300

    Estado das Cadeiras
    Cuida do estado das mesas, contém duas sub-rotinas essenciais:
    
    * HaVagas, que informa se há alguma mesa vaga;
    
    * ObterRota, que retorna um arranjo de pontos para serem seguidos para
    se chegar a mesa

--- Assets/codigos/componentes/estado de jogo/Cadeiras.cs
+++ Assets/codigos/componentes/estado de jogo/Cadeiras.cs
@@ -4,15 +4,96 @@ using UnityEngine;
 
 public class Cadeiras : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public bool podeEntrar;
+    public Transform trRaiz;
+    [Header("Auto Gerado:")]
+    public Transform[] trFolhas;
+    public bool[] b_trFolhas;
+
+#if UNITY_EDITOR
+    [Space(10)]
+    [SerializeField] bool __DEV_AutoValidar;
+#endif // UNITY_EDITOR
+
+    public bool HaVagas() {
+        for (int i = 0; i < b_trFolhas.Length; i++)
+            if (!b_trFolhas[i]) {
+                return true;
+            }
+
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Transform[] ObterRota() {
+        podeEntrar = false;
+        Transform folha = trRaiz;
+
+        for (int i = 0; i < b_trFolhas.Length; i++) {
+            if (!b_trFolhas[i]) {
+                folha = trFolhas[i];
+                b_trFolhas[i] = true;
+                break;
+            }
+        }
+
+        List<Transform> rota = new List<Transform>();
+        BuscaRota(rota, trRaiz, folha);
+
+        return rota.ToArray();
     }
+
+    bool BuscaRota(List<Transform> l, Transform i_tr, Transform folhaAlvo) {
+        l.Add(i_tr);
+
+        int cc = i_tr.childCount;
+
+        if (cc == 0) { // é folha
+            if (i_tr == folhaAlvo) {
+                return true;
+            } else {
+                l.Remove(i_tr);
+                return false;
+            }
+        } else { // galho
+            for (int i = 0; i < cc; i++) {
+                if (BuscaRota(l, i_tr.GetChild(i), folhaAlvo))
+                    return true;
+            }
+            l.Remove(i_tr);
+            return false;
+        }
+    }
+
+    void Start() {
+        StartCoroutine(CO_AtualizaPodeEntrar());
+    }
+
+    IEnumerator CO_AtualizaPodeEntrar() {
+        while(true) {
+            podeEntrar = HaVagas();
+            yield return new WaitForSeconds(Random.Range(1f, 3f));
+        }
+    }
+
+#if UNITY_EDITOR
+    void __DEV_ObtemFolhas(List<Transform> l, Transform i_tr) {
+        int cc = i_tr.childCount;
+
+        if (cc == 0)
+            l.Add(i_tr);
+        else
+            for (int i = 0; i < cc; i++)
+                __DEV_ObtemFolhas(l, i_tr.GetChild(i));
+    }
+
+    void OnValidate() {
+        if (!__DEV_AutoValidar || !trRaiz)
+            return;
+
+        List<Transform> l = new List<Transform>();
+        __DEV_ObtemFolhas(l, trRaiz);
+        trFolhas = l.ToArray();
+        b_trFolhas = new bool[trFolhas.Length];
+    }
+#endif // UNITY_EDITOR
 }
