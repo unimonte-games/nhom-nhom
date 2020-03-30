3c0a60b3c46b50a8764ca73624cbaee2a9f2e331
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-03-30 11:18:58 -0300

    Usando co-rotina em Fila, para ter um pequeno intervalo antes do cliente andar

--- Assets/codigos/componentes/estado de jogo/Fila.cs
+++ Assets/codigos/componentes/estado de jogo/Fila.cs
@@ -11,6 +11,17 @@ public class Fila : MonoBehaviour
         espacosOcupados = new int[fila_trs.Length];
     }
 
+    public void AbrirVaga(int id) {
+        for (int i = 0; i < espacosOcupados.Length; i++) {
+            if (espacosOcupados[i] == id) {
+                espacosOcupados[i] = 0;
+                break;
+            }
+        }
+
+        StartCoroutine(CO_AtualizaLista());
+    }
+
     public int RegistraEspaco(int id) {
         for (int i = 0; i < espacosOcupados.Length; i++) {
             if (espacosOcupados[i] == 0) {
@@ -27,8 +38,19 @@ public class Fila : MonoBehaviour
             return i;
         for (int x = 0; x < espacosOcupados.Length; x++) {
             if (espacosOcupados[x] == id)
-                return i;
+                return x;
         }
         return -1;
+    }
+
+
+    IEnumerator CO_AtualizaLista() {
+        for (int i = 0; i < espacosOcupados.Length-1; i++) {
+            yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+            if (espacosOcupados[i] == 0) {
+                espacosOcupados[i] = espacosOcupados[i+1];
+                espacosOcupados[i+1] = 0;
+            }
+        }
     }
 }
