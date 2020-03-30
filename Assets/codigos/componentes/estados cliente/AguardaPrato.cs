3437da6257a32a20b67ba9708b1443f1a961de60
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-03-30 11:21:49 -0300

    mudanças nos prefabs, cena de teste e iniciando estado AguardaPrato

--- Assets/codigos/componentes/estados cliente/AguardaPrato.cs
+++ Assets/codigos/componentes/estados cliente/AguardaPrato.cs
@@ -2,17 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AguardaPrato : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class AguardaPrato : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
