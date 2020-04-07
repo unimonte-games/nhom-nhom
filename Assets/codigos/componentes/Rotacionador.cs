2d4fccc9aaca18df431178e1f1c924c95fedf6e0
Nicolas <nicolas.andalaft@hotmail.com>
2020-04-07 14:35:47 -0300

    Rotacionador vira para um espaço vazio caso não haja um alvo
    Utilizado no prefab das escadas

--- Assets/codigos/componentes/Rotacionador.cs
+++ Assets/codigos/componentes/Rotacionador.cs
@@ -3,8 +3,10 @@
 namespace NhomNhom {
     public class Rotacionador : MonoBehaviour
     {
+        [Tooltip("ID para o qual o obj deve apontar. Deixe em branco para apontar para um espaço vazio")]
         public string alvo;
 
+        [ContextMenu("Rotação Manual")]
         public void Rotacionar()
         {
             Vector3[] direcoes = new Vector3[4] {
@@ -13,18 +15,22 @@ namespace NhomNhom {
 
             RaycastHit hit;
             GbjID idHit;
+            bool achou = false;
 
-            for (int i = 0; i < direcoes.Length; i++)
+            for (int i = 0; !achou && i < direcoes.Length; i++)
             {
+                // Verifica ID caso encontre um obj
                 if (Physics.Raycast(transform.position, direcoes[i], out hit, 1f))
                 {
-                    idHit = hit.transform.parent.GetComponent<GbjID>();
+                    idHit = hit.transform.GetComponent<GbjID>();
                     if (idHit != null && idHit.id == alvo)
-                    {
-                        transform.rotation = Quaternion.LookRotation(direcoes[i]);
-                        break;
-                    }
+                        achou = true;
                 }
+                // Verifica se não contém alvo (rotaciona para um espaço vazio)
+                else if (alvo == "") achou = true;
+
+                if (achou)
+                    transform.rotation = Quaternion.LookRotation(direcoes[i]);
             }
         }
 
