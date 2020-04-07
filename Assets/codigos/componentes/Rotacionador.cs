8275344575a88bc227fcb044fce64a0cfe38dcf0
Nicolas <nicolas.andalaft@hotmail.com>
2020-04-07 14:49:06 -0300

    Rotacionador aceita multiplos alvos
    Não implementado mas pode ser utilizado no corrimão/ quadro

--- Assets/codigos/componentes/Rotacionador.cs
+++ Assets/codigos/componentes/Rotacionador.cs
@@ -3,7 +3,7 @@
 namespace NhomNhom {
     public class Rotacionador : MonoBehaviour
     {
-        [Tooltip("ID para o qual o obj deve apontar. Deixe em branco para apontar para um espaço vazio")]
+        [Tooltip("Deixe em branco para apontar para um espaço vazio. Para mais de 1 valor separe-os por ';'")]
         public string alvo;
 
         [ContextMenu("Rotação Manual")]
@@ -17,18 +17,27 @@ namespace NhomNhom {
             GbjID idHit;
             bool achou = false;
 
+            string[] alvos = alvo.Split(';');
+
             for (int i = 0; !achou && i < direcoes.Length; i++)
             {
                 // Verifica ID caso encontre um obj
                 if (Physics.Raycast(transform.position, direcoes[i], out hit, 1f))
                 {
                     idHit = hit.transform.GetComponent<GbjID>();
-                    if (idHit != null && idHit.id == alvo)
-                        achou = true;
+                    for (int j = 0; j < alvos.Length; j++)
+                    {
+                        if (idHit.id == alvos[j])
+                        {
+                            achou = true;
+                            break;
+                        }
+                    }
                 }
                 // Verifica se não contém alvo (rotaciona para um espaço vazio)
                 else if (alvo == "") achou = true;
 
+                // Aponta o obj na direção atual
                 if (achou)
                     transform.rotation = Quaternion.LookRotation(direcoes[i]);
             }
