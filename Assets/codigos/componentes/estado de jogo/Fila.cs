1be61e0254890226308a3a84f8a83b2fb9e5484e
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-04-11 15:21:03 -0300

    Corrigindo problema "fura fila", infelizmente ficou menos natural
    agora os clientes andam mais roboticamente na fila, mas ao menos não
    furam a fila.

--- Assets/codigos/componentes/estado de jogo/Fila.cs
+++ Assets/codigos/componentes/estado de jogo/Fila.cs
@@ -62,7 +62,7 @@ namespace NhomNhom {
 
         IEnumerator CO_AtualizaLista() {
             for (int i = 0; i < espacosOcupados.Length-1; i++) {
-                yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+                yield return new WaitForEndOfFrame();
                 if (espacosOcupados[i] == 0) {
                     espacosOcupados[i] = espacosOcupados[i+1];
                     espacosOcupados[i+1] = 0;
