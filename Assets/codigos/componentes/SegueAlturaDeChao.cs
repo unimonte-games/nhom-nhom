8b86d5cbaaf64900e5d76ab23e1ba05ab4569c2a
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-04-22 23:04:18 -0300

    novo componente: SegueAlturaDeChao
    SegueAlturaDeChao é um componente que dispara um raycast de cima para
    baixo para bater numa camada de chão (apesar da especificidade, tudo
    isso é configurável com excessão de que o raycast é sempre pra baixo).
    
    Dessa forma, se o raycast bater com um gbj da camada chão, ele define a
    posição do gbj com o componente como o ponto de colisão + deslocamento,
    ou seja, acaba por ancorar o gbj ao chão.
    
    Esse componente foi feito com o propósito de sempre se alinhar ao chão
    visto que o restaurante contém diferentes alturas

---
+++ Assets/codigos/componentes/SegueAlturaDeChao.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegueAlturaDeChao : MonoBehaviour
+{
+    public Vector3 deslocamento, deslocamentoOrigem;
+    public LayerMask camada;
+
+    Transform tr;
+    Vector3 pos;
+
+    Vector3 up3 = Vector3.up * 3;
+
+    void Awake() {
+        tr = GetComponent<Transform>();
+    }
+
+    void Start() {
+        pos = tr.position;
+    }
+
+    void Update() {
+        pos = tr.position;
+
+        RaycastHit rhit;
+        if (Physics.Raycast(
+            pos + deslocamentoOrigem,
+            Vector3.down,
+            out rhit,
+            100,
+            camada
+        )) {
+            pos = rhit.point + deslocamento;
+            tr.position = pos;
+        }
+    }
+}
