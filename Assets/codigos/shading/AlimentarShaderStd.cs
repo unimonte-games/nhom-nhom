66c81aa7ddfc0273583150b78b05b0ac53c4e213
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-05-04 09:49:29 -0300

    shaders dos slimes e cenário, e um código alimentador

---
+++ Assets/codigos/shading/AlimentarShaderStd.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlimentarShaderStd : MonoBehaviour
+{
+    public Transform luzDirecional, direcaoCamera;
+    public Material[] matsSlimes, matsCenarios;
+
+    void Start() {
+        Atualizar();
+    }
+
+    void OnValidate() {
+        Atualizar();
+    }
+
+    [ContextMenu("Atualizar")]
+    void Atualizar() {
+        if (!luzDirecional | !direcaoCamera || matsSlimes.Length == 0 || matsCenarios.Length == 0)
+            return;
+
+        for (int i = 0; i < matsSlimes.Length; i++) {
+            Material mat = matsSlimes[i];
+            if (mat)
+                mat.SetVector("_light_dir", luzDirecional.forward.normalized * -1);
+        }
+
+        // for (int i = 0; i < matsCenarios.Length; i++) {
+        //     Material mat = matsCenarios[i];
+        //     if (mat)
+        //         mat.SetVector("_cam_dir", direcaoCamera.forward.normalized * 1);
+        // }
+    }
+
+    void OnDrawGizmos() {
+        Atualizar();
+    }
+}
