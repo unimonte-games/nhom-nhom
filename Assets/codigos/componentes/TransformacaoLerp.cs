cffe8473129e316baaf8244ac78f7bf025992fdf
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-05-06 17:50:02 -0300

    Dois novos componentes: Olhador e TransformacaoLerp
    Olhador é basicamente pra fazer LookAt
    TransformacaoLerp define a posição, rotação e escala locais de um
    Transform dentro de um Lerp, usando dois arranjos de tamanho 3.

---
+++ Assets/codigos/componentes/TransformacaoLerp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformacaoLerp : MonoBehaviour
+{
+    public bool ativo;
+    public bool transformarPosicao, transformarRotacao, transformarEscala;
+
+    public Vector3[] vetoresPRE0 = new Vector3[3];
+    public Vector3[] vetoresPRE1 = new Vector3[3];
+
+    [Range(0, 1)] public float t;
+
+    Transform tr;
+
+    void Awake() {
+        tr = GetComponent<Transform>();
+    }
+
+    public void Update() {
+        if (!ativo)
+            return;
+
+        if (transformarPosicao)
+            tr.localPosition = Vector3.Slerp(vetoresPRE0[0], vetoresPRE1[0], t);
+
+        if (transformarRotacao)
+            tr.localEulerAngles = Vector3.Slerp(vetoresPRE0[1], vetoresPRE1[1], t);
+
+        if (transformarEscala)
+            tr.localScale = Vector3.Slerp(vetoresPRE0[2], vetoresPRE1[2], t);
+    }
+}
