28261ecc87195e3b37ca6cad544f4da3a47a5be4
nicolas-ft <nicolas.andalaft@hotmail.com>
2020-05-22 19:01:04 -0300

    HUD das fases criado

--- Assets/codigos/componentes/Cofre.cs
+++ Assets/codigos/componentes/Cofre.cs
@@ -6,10 +6,12 @@ namespace NhomNhom {
     public class Cofre : MonoBehaviour
     {
         public int cofre;
+        public HudLevel hudLevel;
 
         public void Pagar(int recompensa) {
             cofre += recompensa;
-            print("recompensa paga: " + recompensa.ToString());
+            //print("recompensa paga: " + recompensa.ToString());
+            hudLevel.atualizaCofre(cofre);
         }
     }
 }
