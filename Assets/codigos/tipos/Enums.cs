0872a2da124047dbed3f9a0ead62e06e8c2ea5da
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2020-05-25 21:35:31 -0300

    experimento

--- Assets/codigos/tipos/Enums.cs
+++ Assets/codigos/tipos/Enums.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Camadas
-{
-    Piso = 1 << 8,
-}
-
 public enum EfeitoSonoro {
     PedidoNovo = 0,
     SlimePasso,
     Pago,
     PedidoEntregue,
 }
+
+public enum Camadas
+{
+    Piso = 1 << 8,
+}
+
