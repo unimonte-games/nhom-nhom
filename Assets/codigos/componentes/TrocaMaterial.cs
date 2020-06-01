4e9a0f7c11c4be45fed6b406050a7aba9975cd4c
nicolas-ft <nicolas.andalaft@hotmail.com>
2020-06-01 15:42:52 -0300

    Adicionados sprites do balcao e sistema de aleatorizar um material

---
+++ Assets/codigos/componentes/TrocaMaterial.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class TrocaMaterial : MonoBehaviour
+{
+    public Material[] materiais;
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        TrocarMaterial();
+    }
+
+    public void TrocarMaterial()
+    {
+        int random = Random.Range(0, materiais.Length - 1);
+        meshRenderer.material = materiais[random];
+    }
+}
